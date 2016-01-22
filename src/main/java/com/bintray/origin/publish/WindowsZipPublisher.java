bc3d06c0651557d6ab6a951a49eb6702c2475c9f
Matthias Cullmann <matthias.cullmann@gmail.com>
2016-01-22 21:59:37 +0100

    disbale windows

--- src/main/java/com/bintray/origin/publish/WindowsZipPublisher.java
+++ src/main/java/com/bintray/origin/publish/WindowsZipPublisher.java
@@ -8,7 +8,7 @@ public class WindowsZipPublisher implements RepackagingPublisher {
 
 	@Override
 	public boolean isActiveFor(JDKDescriptor desc) {
-		return "windows".equals(desc.getOs());
+		return false &&  "windows".equals(desc.getOs());
 	}
 
 	public Repack getRepacker() {
